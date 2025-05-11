aa51bd62f04f771b0b45905b603ed3330521ce31
Melis Yıldız <melisyildiz707@gmail.com>
2025-05-11 13:06:31 +0300

    application.properties dosyası sadeleştirildi

--- LibraryAppSpringBoot/src/main/java/com/alisimsek/LibraryManagementProject/config/CorsConfig.java
+++ LibraryAppSpringBoot/src/main/java/com/alisimsek/LibraryManagementProject/config/CorsConfig.java
@@ -2,9 +2,6 @@ package com.alisimsek.LibraryManagementProject.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,30 +13,16 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
+                registry.addMapping("/")
                         .allowedOriginPatterns(
-                                "http://localhost:8080",
-                                "https://advisory-slug-frivoller-95937079.koyeb.app",
-                                "http://advisory-slug-frivoller-95937079.koyeb.app",
-                                "https://lovely-bublanina-452bec.netlify.app"
+                                "http://localhost:8080", 
+                                "https://advisory-slug-frivoller-95937079.koyeb.app", 
+                                "https://advisory-slug-frivoller-95937079.koyeb.app"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
-        };
-    }
-
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(false);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        config.addExposedHeader("*");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
+        };
+    }
 }
